9199efe8fc7140bfcc97863fd4306e0e0046cab6
kmoonwright <kmoonwright@outlook.com>
2019-09-23 14:05:19 -0700

    Render proper nav bars with styling
    Co-authored-by: long <35038654+uwgnol1612@users.noreply.github.com>

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -3,18 +3,24 @@ import React from 'react';
 import SearchContainer from '../nav/search_container'
 import logo from '../nav/assets/logo.png'
 
+import NavMainContainer from './navbar_main_container';
+
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div className="landing-page-container">
-        <div className="landing-page-logo-container">
-          <div className="landing-page-logo-items">
-            <img src={logo} alt="logo" />
+        <div className="landing-page-container">
+          <div>
+            <NavMainContainer />
           </div>
-          <SearchContainer />
+          <div className="landing-page-logo-container">
+            <div className="landing-page-logo-items">
+              <img src={logo} alt="logo" />
+            </div>
+            <SearchContainer />
+          </div>
         </div>
-      </div>
+
     );
   }
 }
---
+++ frontend/src/components/main/navbar_main.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom'
+
+import SearchContainer from '../nav/search_container'
+import logo from '../nav/assets/logo.png'
+import Modal from '../modal/modal';
+
+// import '../nav/assets/navbar.css'
+// import './assets/navbar.css';
+// import SearchContainer from './search_container'
+
+class NavBarMain extends React.Component {
+    constructor(props) {
+        super(props);
+        this.logoutUser = this.logoutUser.bind(this);
+        this.getLinks = this.getLinks.bind(this);
+    }
+
+    logoutUser(e) {
+        e.preventDefault();
+        this.props.logout();
+        this.props.history.push('/');
+    }
+
+    // Selectively render links dependent on whether the user is logged in
+    getLinks() {
+        if (this.props.loggedIn) {
+            return (
+                <div className="nav-bar-main-logged-in">
+                    <div>
+                        <button><Link to={"/profile"}>Profile</Link></button>&nbsp;&nbsp;
+                        <button onClick={this.logoutUser}>Logout</button>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="nav-bar-main-logged-out">
+                    <div>
+                        <button className="navbutton" onClick={() => this.props.openModal('signup')}>Sign up</button>
+                        <button className="navbutton" onClick={() => this.props.openModal('login')}>Login</button>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="nav-bar-main-container">
+                {/* <div className="nav-bar-items"> */}
+                {this.getLinks()}
+                {/* </div> */}
+            </div>
+        );
+    }
+
+}
+
+export default withRouter(NavBarMain);
