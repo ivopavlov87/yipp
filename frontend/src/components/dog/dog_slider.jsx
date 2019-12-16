e82412e44d83518ccbb46935beb3c51cd7cfcde1
Long McFarlin <longwu2011@gmail.com>
2019-12-16 12:44:08 -0700

    fix no image to display for some dogs

--- frontend/src/components/dog/dog_slider.jsx
+++ frontend/src/components/dog/dog_slider.jsx
@@ -9,15 +9,23 @@ class DogSlider extends React.Component {
             slidesToShow: 3,
             slidesToScroll: 3
         }
-        const photos = this.props.imgUrls.map((url, idx) =>
-            <div className='image-slide' key={`dogImg-${idx}`}>
-                <img src={url} alt='dog-show' />
-            </div>)
-        return (
-            <Slider {...settings}>
-                {photos}
-            </Slider>
-        )
+        
+        if (this.props.imgUrls.length === 0) {
+            return <div className="no-image-display">
+                <img src="https://we-camp-seeds.s3.us-east-2.amazonaws.com/no-image.png" alt="no-img-display"/>
+            </div>
+        } else {
+            let photos = this.props.imgUrls.map((url, idx) =>
+                <div className='image-slide' key={`dogImg-${idx}`}>
+                    <img src={url} alt='dog-show' />
+                </div>)
+            return (
+                <Slider {...settings}>
+                    {photos}
+                </Slider>
+            )
+        }
+        
     }
 }
 
